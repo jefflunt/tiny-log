2ca396d4c2f06e78b466921cc183f0d546237a6c
jefflunt <jefflunt@gmail.com>
2022-11-19 11:31:19 -0600

    add optional buffering flag to allow non-buffered output, which is preferable for dev/test environments

--- lib/tiny_log.rb
+++ lib/tiny_log.rb
@@ -16,7 +16,12 @@ class TinyLog
   # filename: the I/O stream to send log messages to
   #   if unspecified, will default to $stdout
   #   if specified, attempts to open a file with the specified name to append to
-  def initialize(filename=nil)
+  # buffered: whether or not to buffer log output
+  #   true, or not specified: log buffering is enabled
+  #   false: log buffering is disabled - most useful for dev/test environments
+  #     where you want to watch output in realtime
+  def initialize(filename=nil, buffering=true)
+    @buffering = !!buffering
     @io = filename.is_a?(String) ? File.open(filename, 'a') : $stdout
   end
 
@@ -28,6 +33,8 @@ class TinyLog
         @io.puts "#{Time.now.utc.iso8601(6)} #{Process.pid.to_s.rjust(6)} #{prefix.to_s.upcase} #{l}"
       end
     end
+
+    @io.flush unless @buffering
 
     nil
   end
