48fc167c98c7866575cc31972587f258e9bca720
jefflunt <jefflunt@gmail.com>
2023-01-21 16:24:42 -0600

    allow background thread for writing messages in order to avoid blocking the main thread

--- lib/tiny_log.rb
+++ lib/tiny_log.rb
@@ -16,26 +16,42 @@ class TinyLog
   # filename: the I/O stream to send log messages to
   #   if unspecified, will default to $stdout
   #   if specified, attempts to open a file with the specified name to append to
-  # buffered: whether or not to buffer log output
+  # buffering: whether or not to buffer log output
   #   true, or not specified: log buffering is enabled
   #   false: log buffering is disabled - most useful for dev/test environments
   #     where you want to watch output in realtime
-  def initialize(filename=nil, buffering=true)
+  # background_thread: whether logging should be done in a background Thread
+  #   true, or not specified, use a Thread to write to the log every 5 seconds
+  #   false, write to the log immediately
+  def initialize(filename: nil, buffering: true, background_thread: true)
     @buffering = !!buffering
     @io = filename.is_a?(String) ? File.open(filename, 'a') : $stdout
+
+    if background_thread
+      @msg_queue = Queue.new
+      @background_thread = Thread.new do |t|
+        loop do
+          @msg_queue
+            .length
+            .times{ @io.puts @msg_queue.shift }
+          sleep 5
+        end
+      end
+    end
   end
 
   # the clever bit that annotates the log message with a log level and UTC
   # timestamp
   def method_missing(prefix, *msgs)
-    msgs.each do |m|
-      m.to_s.lines.each do |l|
-        @io.puts "#{Time.now.utc.iso8601(6)} #{Process.pid.to_s.rjust(6)} #{prefix.to_s.upcase} #{l}"
-      end
-    end
-
+    msgs.each{|m| _build_lines(m).each{|l| @background_thread ? @msg_queue << l : @io.puts(l) } }
     @io.flush unless @buffering
 
     nil
+  end
+
+  def _build_lines(m)
+    m.to_s.lines.map do |l|
+      "#{Time.now.utc.iso8601(6)} #{Process.pid.to_s.rjust(6)} #{prefix.to_s.upcase} #{l}"
+    end
   end
 end
